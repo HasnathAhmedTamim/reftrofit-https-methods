b4279a6f4017c9d95bc6c6c9940aca0a54dd7d25
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-20 12:15:34 +0600

    Use Case for getting all posts Represents single business operation Makes code reusable and testable

---
+++ app/src/main/java/com/example/jsonplaceholderpractice/domain/usecase/GetAllPostsUseCase.kt
@@ -0,0 +1,20 @@
+package com.example.jsonplaceholderpractice.domain.usecase
+
+import com.example.jsonplaceholderpractice.domain.model.Post
+import com.example.jsonplaceholderpractice.domain.repository.PostRepository
+import com.example.jsonplaceholderpractice.utils.Resource
+
+
+/**
+ * Use Case for getting all posts
+ * Represents single business operation
+ * Makes code reusable and testable
+ */
+
+class GetAllPostsUseCase(
+    private  val repository: PostRepository
+){
+    suspend operator fun invoke(): Resource<List<Post>> {
+        return repository.getPosts()
+    }
+}
