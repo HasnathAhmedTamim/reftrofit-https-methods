5b8d3ef3ad5bc1dc6eb30bfedbd2d04a4f1f17f3
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-20 12:08:33 +0600

    create post repository data implementation

---
+++ app/src/main/java/com/example/jsonplaceholderpractice/domain/repository/PostRepository.kt
@@ -0,0 +1,44 @@
+package com.example.jsonplaceholderpractice.domain.repository
+
+
+import com.example.jsonplaceholderpractice.domain.model.Post
+import com.example.jsonplaceholderpractice.utils.Resource
+
+/**
+ * Repository interface (contract)
+ * Defines WHAT operations are available
+ * Actual implementation is in data layer
+ */
+
+interface PostRepository{
+    // Get all posts from API
+    suspend fun getPosts(): Resource<List<Post>>
+
+    // Get single post by ID
+    suspend fun getPostById(postId: Int): Resource<Post>
+
+    // Create new post
+    suspend fun createPost(
+        userId: Int,
+        title: String,
+        body: String
+    ): Resource<Post>
+
+    // Update existing post
+    suspend fun updatePost(
+        id: Int,
+        userId: Int,
+        title: String,
+        body: String
+    ): Resource<Post>
+
+    // Delete post byID
+    suspend fun deletePost(id: Int): Resource<Boolean>
+}
+
+
+/**
+ * What is suspend?: Marks function as "suspendable" - can be paused and resumed.
+ * Used with Kotlin coroutines for async operations without blocking the UI.
+ *
+ * */
